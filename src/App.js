cff7a8dd6a370996156b8cecbd55df26ea717f18
horseman7677 <horseman7677@gmail.com>
2022-04-01 23:23:19 +0530

    setup finished

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
       <h1>hey there I'm horseman.....</h1>
+      {/* Header */}
+      {/* Tinder card */}
+      {/* Footer */}
     </div>
   );
 }
